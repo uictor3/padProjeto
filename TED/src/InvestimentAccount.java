a1a93d92fc4e1ba4df36b4c784ef7e0bd642e8cd
wictor costa <142526733+uictor3@users.noreply.github.com>
2024-03-25 19:35:30 -0300

    InvestimentAccount.java

--- TED/src/InvestimentAccount.java
+++ TED/src/InvestimentAccount.java
@@ -3,13 +3,13 @@ public class InvestimentAccount extends AccountSystem{
     double taxa = 0.1;
     int deadLine;
 
-    public double remdimento(double taxa){
+    public double rendimento(double taxa){
         this.saldo+= this.saldo * taxa;
         return this.saldo;
     }
     boolean sacar(double valor){
         if (this.saldo >= valor){
-            this.saldo = this.saldo - valor;
+            this.saldo = this.saldo - (valor * 1.01);
             return true;
         }else {
             return false;
@@ -18,7 +18,7 @@ public class InvestimentAccount extends AccountSystem{
     }
     boolean depositar(double valor) {
         if (valor >= 0) {
-            this.saldo = this.saldo + valor;
+            this.saldo = this.saldo + (valor * 1.01);
             return true;
         } else{
             return false;
